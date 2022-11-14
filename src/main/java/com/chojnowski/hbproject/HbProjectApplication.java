a75835ead7159ae505ea4fcc5f99382550b38d0f
CezaryChojnowski <chojnowski.czarek@gmail.com>
2022-11-14 22:32:03 +0100

    Initial commit

---
+++ src/main/java/com/chojnowski/hbproject/HbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chojnowski.hbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbProjectApplication.class, args);
+    }
+
+}
