8c9afb461d1ab19ee402bfa3ea842a9a797b37e9
CezaryChojnowski <chojnowski.czarek@gmail.com>
2022-11-20 15:34:31 +0100

    Interface with extract and map to entries methods

---
+++ src/main/java/com/chojnowski/hbproject/transactions_brockers/transfer_files/logic/ImportedFilesManipulateService.java
@@ -0,0 +1,11 @@
+package com.chojnowski.hbproject.transactions_brockers.transfer_files.logic;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface ImportedFilesManipulateService<T1,R1> {
+    List<T1> extractObject(MultipartFile file);
+    List<R1> mapToEntities(List<T1> extractedObject);
+
+}
