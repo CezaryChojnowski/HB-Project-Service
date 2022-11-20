d7a72935620d9be28599d25608c8fbb432c2d32a
CezaryChojnowski <chojnowski.czarek@gmail.com>
2022-11-20 15:32:28 +0100

    Csv service to handle reading from csv file

---
+++ src/main/java/com/chojnowski/hbproject/service/util_service/csv/CsvOption.java
@@ -0,0 +1,10 @@
+package com.chojnowski.hbproject.service.util_service.csv;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class CsvOption {
+    private char separator;
+}
---
+++ src/main/java/com/chojnowski/hbproject/service/util_service/csv/CsvService.java
@@ -0,0 +1,31 @@
+package com.chojnowski.hbproject.service.util_service.csv;
+
+import com.chojnowski.hbproject.transactions_brockers.transfer_files.implementations.pko_bp.model.PkoCsv;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.List;
+
+@Component
+public class CsvService<T> {
+    private final Class<? extends T> T = (Class<? extends T>) PkoCsv.class;
+//    private Class<T> tClass;
+//
+//    public CsvService(Class<T> tClass) {
+//        this.tClass = tClass;
+//    }
+
+    @SneakyThrows
+    public List<T> readCsvFile(MultipartFile multipartfile, CsvOption csvOption) throws FileNotFoundException {
+        Reader reader = new InputStreamReader(multipartfile.getInputStream());
+        CsvToBean<T> csvtobean= new CsvToBeanBuilder<T>(reader)
+                .withType(T)
+                .withSeparator(csvOption.getSeparator())
+                .build();
+        return csvtobean.parse();
+    }
+}
