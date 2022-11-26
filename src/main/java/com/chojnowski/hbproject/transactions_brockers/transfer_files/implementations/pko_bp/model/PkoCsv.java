235523205339a2b4793abef77aa5f5be56589e33
CezaryChojnowski <chojnowski.czarek@gmail.com>
2022-11-26 23:13:51 +0100

    Change access modifiers in PkoCsv class

--- src/main/java/com/chojnowski/hbproject/transactions_brockers/transfer_files/implementations/pko_bp/model/PkoCsv.java
+++ src/main/java/com/chojnowski/hbproject/transactions_brockers/transfer_files/implementations/pko_bp/model/PkoCsv.java
@@ -1,11 +1,11 @@
 package com.chojnowski.hbproject.transactions_brockers.transfer_files.implementations.pko_bp.model;
 
 
-import com.chojnowski.hbproject.entity.CardOperation;
 import com.chojnowski.hbproject.transactions_brockers.transfer_files.implementations.pko_bp.model.converter.LocalDateConverter;
 import com.opencsv.bean.CsvBindByPosition;
 import com.opencsv.bean.CsvCustomBindByPosition;
 import lombok.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -20,44 +20,55 @@ import java.time.LocalDate;
 public class PkoCsv {
 
     @CsvCustomBindByPosition(position = 0, converter = LocalDateConverter.class)
-    public LocalDate operationDate;
+    private LocalDate operationDate;
 
     @CsvCustomBindByPosition(position = 1, converter = LocalDateConverter.class)
-    public LocalDate valueDate;
+    private LocalDate valueDate;
 
     @CsvBindByPosition(position = 2)
-    public String typeOperation;
+    private String typeOperation;
 
     @CsvBindByPosition(position = 3)
-    public BigDecimal amount;
+    private BigDecimal amount;
 
     @CsvBindByPosition(position = 4)
-    public String currency;
+    private String currency;
 
     @CsvBindByPosition(position = 5)
-    public BigDecimal balanceAfterOperation;
+    private BigDecimal balanceAfterOperation;
 
     @CsvBindByPosition(position = 6)
-    public String description;
+    private String description;
 
     @CsvBindByPosition(position = 7)
-    public String details1;
+    private String details1;
 
     @CsvBindByPosition(position = 8)
-    public String details2;
+    private String details2;
 
     @CsvBindByPosition(position = 9)
-    public String details3;
+    private String details3;
 
     @CsvBindByPosition(position = 10)
-    public String details4;
+    private String details4;
 
     @CsvBindByPosition(position = 11)
-    public String details5;
+    private String details5;
 
     @CsvBindByPosition(position = 12)
-    public String details6;
+    private String details6;
 
-    @CsvBindByPosition(position = 13)
-    public String details7;
+    private String concatenatedIrregularField;
+
+    public void concatenateIrregularField(){
+        this.concatenatedIrregularField = String.format("%s|%s|%s|%s|%s|%s|%s",
+                StringUtils.trim(this.description),
+                StringUtils.trim(this.details1),
+                StringUtils.trim(this.details2),
+                StringUtils.trim(this.details3),
+                StringUtils.trim(this.details4),
+                StringUtils.trim(this.details5),
+                StringUtils.trim(this.details6)
+        );
+    }
 }
