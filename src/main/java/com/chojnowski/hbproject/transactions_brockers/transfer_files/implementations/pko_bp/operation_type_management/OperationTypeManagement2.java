8b1ff185676f2134304ea68cbbd1267c184c587d
CezaryChojnowski <chojnowski.czarek@gmail.com>
2022-11-26 23:09:57 +0100

    Remove unused classes

--- src/main/java/com/chojnowski/hbproject/transactions_brockers/transfer_files/implementations/pko_bp/operation_type_management/OperationTypeManagement2.java
+++
@@ -1,7 +0,0 @@
-package com.chojnowski.hbproject.transactions_brockers.transfer_files.implementations.pko_bp.operation_type_management;
-
-/**
- * @author cchojnowski
- */
-public interface OperationTypeManagement2 {
-}
